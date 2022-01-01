44deb93356960694cd79d3af6e4c7b760c909c8d
rvcas <x@rvcas.dev>
2022-01-01 16:49:48 -0500

    tests and allocating

--- build.zig
+++ build.zig
@@ -11,20 +11,6 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("lambda", "src/main.zig");
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
-
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
-
     const exe_tests = b.addTest("src/main.zig");
     exe_tests.setTarget(target);
     exe_tests.setBuildMode(mode);
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,9 @@
 const std = @import("std");
 
 const Expr = union(enum) {
-    variable: [*]u8,
+    variable: []const u8,
     lambda: struct {
-        name: [*]u8,
+        name: []const u8,
         body: *Expr,
     },
     application: struct {
@@ -14,51 +14,98 @@ const Expr = union(enum) {
 
 const Value = union(enum) {
     int: usize,
-    str: [*]u8,
+    str: []const u8,
     closure: struct {
-        name: [*]u8,
+        name: []const u8,
         body: *Expr,
         env: *Env,
     },
     err: []const u8,
 };
 
-const Env = std.AutoHashMap([]u8, Value);
+const Env = std.StringHashMap(*Value);
 
-fn eval(expr: *Expr, env: *Env) Value {
-    return switch (expr) {
-        .variable => |variable| env.get(variable) orelse .{
-            .err = "Variable is not defined",
+fn eval(expr: *Expr, env: *Env, allocator: std.mem.Allocator) *Value {
+    return switch (expr.*) {
+        .variable => |variable| blk: {
+            if (env.get(variable)) |name| {
+                break :blk name;
+            } else {
+                var err = allocator.create(Value) catch unreachable;
+
+                err.* = Value{ .err = "Variable is not defined" };
+
+                break :blk err;
+            }
         },
-        .lambda => |lambda| .{
-            .closure = .{
-                .name = lambda.name,
-                .body = lambda.body,
-                .env = env,
-            },
+        .lambda => |lambda| blk: {
+            var closure = allocator.create(Value) catch unreachable;
+
+            closure.* = Value{
+                .closure = .{
+                    .name = lambda.name,
+                    .body = lambda.body,
+                    .env = env,
+                },
+            };
+
+            break :blk closure;
         },
-        .application => |application| call(
-            eval(application.operator, env),
-            eval(application.operand, env),
-        ),
+        .application => |application| blk: {
+            var left = eval(application.operator, env, allocator);
+            var right = eval(application.operand, env, allocator);
+
+            break :blk call(left, right, allocator);
+        },
     };
 }
 
-fn call(function: Value, argument: Value) Value {
-    return switch (function) {
+fn call(function: *Value, argument: *Value, allocator: std.mem.Allocator) *Value {
+    return switch (function.*) {
         .closure => |closure| blk: {
-            closure.env.put(closure.name, argument);
+            closure.env.put(closure.name, argument) catch unreachable;
 
-            break :blk eval(closure.body, closure.env);
+            break :blk eval(closure.body, closure.env, allocator);
         },
-        else => .{ .err = "Only functions can be called" },
+        else => blk: {
+            var err = allocator.create(Value) catch unreachable;
+
+            err.* = Value{ .err = "Only functions can be called" };
+
+            break :blk err;
+        },
     };
 }
 
-pub fn main() anyerror!void {
-    std.log.info("All your codebase are belong to us.", .{});
-}
+const testing = std.testing;
 
-test "basic test" {
-    try std.testing.expectEqual(10, 3 + 7);
+test "calculus" {
+    var env = Env.init(testing.allocator);
+    defer env.deinit();
+
+    var expr = try testing.allocator.create(Expr);
+    defer testing.allocator.destroy(expr);
+
+    expr.* = Expr{ .variable = "x" };
+
+    const undefined_result = eval(expr, &env, testing.allocator);
+    defer testing.allocator.destroy(undefined_result);
+
+    try std.testing.expectEqual(
+        Value{ .err = "Variable is not defined" },
+        undefined_result.*,
+    );
+
+    var int_value = try testing.allocator.create(Value);
+    int_value.* = Value{ .int = 123 };
+
+    try env.put("x", int_value);
+
+    const int_result = eval(expr, &env, testing.allocator);
+    defer testing.allocator.destroy(int_result);
+
+    try std.testing.expectEqual(
+        Value{ .int = 123 },
+        int_result.*,
+    );
 }
